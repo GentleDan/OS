bf05dc1b78184d224eddf47641f1e20a1fcc978d
Камышов Данила <ilovekek@mail.ru>
2020-11-18 17:41:27 +0400

    commit

---
+++ Laba_4/MoveSelectionWindow.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+public class MoveSelectionWindow extends JDialog {
+    private final JTree tree;
+    private DefaultMutableTreeNode selectedNode;
+    private boolean accept;
+
+    public MoveSelectionWindow(JFrame owner, DefaultMutableTreeNode root) {
+        super(owner, true);
+        setLayout(null);
+        tree = new JTree(root);
+        setBounds(0, 0, 900, 900);
+        tree.setBounds(0, 0, 400, 500);
+        tree.addTreeSelectionListener(e1 -> selectedNode = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent());
+        add(tree);
+
+        JButton acceptButton = new JButton("Accept");
+        acceptButton.setBounds(0, 500, 90, 90);
+        acceptButton.addActionListener(e -> {
+            accept = true;
+            dispose();
+        });
+        add(acceptButton);
+        JButton cancelButton = new JButton("Cancel");
+        cancelButton.setBounds(0, 600, 90, 90);
+        cancelButton.addActionListener(e -> dispose());
+        add(cancelButton);
+        tree.updateUI();
+        repaint();
+        setVisible(true);
+    }
+
+    public DefaultMutableTreeNode getNode() {
+        if (selectedNode != null && selectedNode.getAllowsChildren() && accept) {
+            return selectedNode;
+        }
+        return null;
+    }
+}
---
+++ Laba_4/Panel.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Panel extends JPanel {
+
+    private Disk disk;
+
+    public void setDisk(Disk disk) {
+        this.disk = disk;
+    }
+
+    public void paint(Graphics g) {
+        if (disk != null) {
+            int size = 20;
+            int padding = 200;
+            super.paint(g);
+            int x = padding;
+            int y = padding / 4;
+
+            for (int i = 0; i < disk.getMemoryCapacity(); i++) {
+                if (x + size >= getWidth()) {
+                    x = padding;
+                    y += size;
+                }
+                switch (disk.getCells()[i].getCellStatus()) {
+                    case 0 -> g.setColor(Color.GRAY);
+                    case 1 -> g.setColor(Color.BLUE);
+                    case 2 -> g.setColor(Color.RED);
+                }
+                g.fillRect(x, y, size, size);
+                g.setColor(Color.black);
+                g.drawRect(x, y, size, size);
+                x += size;
+            }
+        }
+    }
+}
